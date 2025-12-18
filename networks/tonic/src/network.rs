217e0942ce38af4b5eae9badd394da4a26ad11c2
张林伟 <lewiszlw520@gmail.com>
2025-12-18 16:06:30 +0800

    Add ser/de code

--- networks/tonic/src/network.rs
+++ networks/tonic/src/network.rs
@@ -1,16 +1,64 @@
+use std::sync::Arc;
+
+use datafusion::physical_plan::ExecutionPlan;
 use datafusion_dist::{
     DistError, DistResult, RecordBatchStream,
     cluster::NodeId,
     network::{DistNetwork, ScheduledTasks, StageId, TaskId},
     util::get_local_ip,
 };
+use datafusion_proto::{
+    physical_plan::{AsExecutionPlan, PhysicalExtensionCodec},
+    protobuf::PhysicalPlanNode,
+};
 use tonic::transport::Endpoint;
 
-use crate::protobuf::{self, SendTasksReq, dist_tonic_service_client::DistTonicServiceClient};
+use crate::protobuf::{
+    self, SendTasksReq, StagePlan, dist_tonic_service_client::DistTonicServiceClient,
+};
 
 #[derive(Debug)]
 pub struct DistTonicNetwork {
-    port: u16,
+    pub port: u16,
+    pub extension_codec: Arc<dyn PhysicalExtensionCodec>,
+}
+
+impl DistTonicNetwork {
+    fn serialize_scheduled_tasks(
+        &self,
+        scheduled_tasks: ScheduledTasks,
+    ) -> DistResult<SendTasksReq> {
+        let mut proto_stage_plans = Vec::new();
+        for (stage_id, plan) in scheduled_tasks.stage_plans {
+            proto_stage_plans.push(self.serialize_stage_plan(stage_id, plan)?);
+        }
+        let proto_task_ids = scheduled_tasks
+            .task_ids
+            .into_iter()
+            .map(serialize_task_id)
+            .collect::<Vec<_>>();
+
+        Ok(SendTasksReq {
+            stage_plans: proto_stage_plans,
+            tasks: proto_task_ids,
+        })
+    }
+
+    fn serialize_stage_plan(
+        &self,
+        stage_id: StageId,
+        plan: Arc<dyn ExecutionPlan>,
+    ) -> DistResult<StagePlan> {
+        let proto_stage_id = serialize_stage_id(stage_id);
+        let mut plan_buf: Vec<u8> = vec![];
+        let plan_proto =
+            PhysicalPlanNode::try_from_physical_plan(plan, self.extension_codec.as_ref())?;
+        plan_proto.try_encode(&mut plan_buf)?;
+        Ok(StagePlan {
+            stage_id: Some(proto_stage_id),
+            plan: plan_buf,
+        })
+    }
 }
 
 #[async_trait::async_trait]
@@ -31,7 +79,7 @@ impl DistNetwork for DistTonicNetwork {
             .map_err(|e| DistError::network(Box::new(e)))?;
         let mut tonic_client =
             DistTonicServiceClient::new(channel).max_encoding_message_size(usize::MAX);
-        let send_tasks_req = serialize_scheduled_tasks(scheduled_tasks)?;
+        let send_tasks_req = self.serialize_scheduled_tasks(scheduled_tasks)?;
         tonic_client
             .send_tasks(send_tasks_req)
             .await
@@ -48,18 +96,14 @@ impl DistNetwork for DistTonicNetwork {
     }
 }
 
-fn serialize_scheduled_tasks(_scheduled_tasks: ScheduledTasks) -> DistResult<SendTasksReq> {
-    todo!()
-}
-
-fn _serialize_stage_id(stage_id: StageId) -> protobuf::StageId {
+fn serialize_stage_id(stage_id: StageId) -> protobuf::StageId {
     protobuf::StageId {
         job_id: stage_id.job_id.to_string(),
         stage: stage_id.stage,
     }
 }
 
-fn _serialize_task_id(task_id: TaskId) -> protobuf::TaskId {
+fn serialize_task_id(task_id: TaskId) -> protobuf::TaskId {
     protobuf::TaskId {
         job_id: task_id.job_id.to_string(),
         stage: task_id.stage,
--- networks/tonic/src/server.rs
+++ networks/tonic/src/server.rs
@@ -1,32 +1,68 @@
+use std::{collections::HashMap, sync::Arc};
+
+use datafusion::{physical_plan::ExecutionPlan, prelude::SessionContext};
 use datafusion_dist::{
+    DistResult,
     network::{ScheduledTasks, StageId, TaskId},
     runtime::DistRuntime,
+};
+use datafusion_proto::{
+    physical_plan::{AsExecutionPlan, PhysicalExtensionCodec},
+    protobuf::PhysicalPlanNode,
 };
 use tonic::{Request, Response, Status};
 use uuid::Uuid;
 
 use crate::protobuf::{
-    self, SendTasksReq, SendTasksResp, dist_tonic_service_server::DistTonicService,
+    self, SendTasksReq, SendTasksResp, StagePlan, dist_tonic_service_server::DistTonicService,
 };
 
+pub struct DistTonicServer {
+    pub runtime: Arc<DistRuntime>,
+    pub ctx: SessionContext,
+    pub extension_codec: Arc<dyn PhysicalExtensionCodec>,
+}
+
+impl DistTonicServer {
+    fn parse_send_tasks_req(&self, req: SendTasksReq) -> DistResult<ScheduledTasks> {
+        let stage_plans = req
+            .stage_plans
+            .into_iter()
+            .map(|p| self.parse_stage_plan(p))
+            .collect::<DistResult<HashMap<_, _>>>()?;
+        let task_ids = req.tasks.into_iter().map(parse_task_id).collect::<Vec<_>>();
+        Ok(ScheduledTasks::new(stage_plans, task_ids))
+    }
+
+    fn parse_stage_plan(&self, proto: StagePlan) -> DistResult<(StageId, Arc<dyn ExecutionPlan>)> {
+        let stage_id = parse_stage_id(proto.stage_id.expect("stage_id should not be null"));
+        let plan: Arc<dyn ExecutionPlan> =
+            PhysicalPlanNode::try_decode(&proto.plan).and_then(|proto| {
+                proto.try_into_physical_plan(
+                    &self.ctx,
+                    &self.ctx.runtime_env(),
+                    self.extension_codec.as_ref(),
+                )
+            })?;
+        Ok((stage_id, plan))
+    }
+}
+
 #[async_trait::async_trait]
-impl DistTonicService for DistRuntime {
+impl DistTonicService for DistTonicServer {
     async fn send_tasks(
         &self,
         request: Request<SendTasksReq>,
     ) -> Result<Response<SendTasksResp>, Status> {
-        let scheduled_tasks = parse_send_tasks_req(request.into_inner())?;
-        self.receive_tasks(scheduled_tasks).await;
+        let scheduled_tasks = self
+            .parse_send_tasks_req(request.into_inner())
+            .map_err(|e| Status::internal(format!("Failed to parse SendTasksReq: {e}")))?;
+        self.runtime.receive_tasks(scheduled_tasks).await;
         Ok(Response::new(SendTasksResp {}))
     }
 }
 
-#[allow(clippy::result_large_err)]
-fn parse_send_tasks_req(_req: SendTasksReq) -> Result<ScheduledTasks, Status> {
-    todo!()
-}
-
-fn _parse_stage_id(proto: protobuf::StageId) -> StageId {
+fn parse_stage_id(proto: protobuf::StageId) -> StageId {
     let job_id = Uuid::parse_str(&proto.job_id)
         .unwrap_or_else(|_| panic!("Failed to parse job id {} as uuid", proto.job_id));
     StageId {
@@ -35,7 +71,7 @@ fn _parse_stage_id(proto: protobuf::StageId) -> StageId {
     }
 }
 
-fn _parse_task_id(proto: protobuf::TaskId) -> TaskId {
+fn parse_task_id(proto: protobuf::TaskId) -> TaskId {
     let job_id = Uuid::parse_str(&proto.job_id)
         .unwrap_or_else(|_| panic!("Failed to parse job id {} as uuid", proto.job_id));
     TaskId {
