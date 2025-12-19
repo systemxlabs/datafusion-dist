7cf82db78d40dfe32f3e4e53e905df4dc75f3cea
张林伟 <lewiszlw520@gmail.com>
2025-12-19 11:12:47 +0800

    Implement flight sql service for test app

--- integration-tests/app/src/main.rs
+++ integration-tests/app/src/main.rs
@@ -1,22 +1,32 @@
-use std::{error::Error, io::Cursor, pin::Pin, sync::Arc};
+use std::{collections::HashMap, error::Error, pin::Pin, sync::Arc};
 
 use arrow_flight::{
-    Action, ActionType, Criteria, Empty, FlightData, FlightDescriptor, FlightInfo,
-    HandshakeRequest, HandshakeResponse, PollInfo, PutResult, SchemaResult, Ticket,
+    FlightDescriptor, FlightEndpoint, FlightInfo, HandshakeRequest, HandshakeResponse, SchemaAsIpc,
+    Ticket,
     encode::FlightDataEncoderBuilder,
     error::FlightError,
+    flight_descriptor::DescriptorType,
     flight_service_server::{FlightService, FlightServiceServer},
+    sql::{Any, CommandStatementQuery, ProstMessageExt, SqlInfo, server::FlightSqlService},
 };
-use datafusion::{arrow::ipc::writer::IpcWriteOptions, prelude::SessionContext};
-use datafusion_dist::runtime::DistRuntime;
+use datafusion::{
+    arrow::{
+        datatypes::SchemaRef,
+        ipc::writer::{DictionaryTracker, IpcDataGenerator, IpcWriteOptions},
+    },
+    physical_plan::display::DisplayableExecutionPlan,
+    prelude::SessionContext,
+};
+use datafusion_dist::{cluster::NodeId, network::TaskId, runtime::DistRuntime};
 use datafusion_dist_cluster_postgres::PostgresClusterBuilder;
 use datafusion_dist_network_tonic::{
-    network::DistTonicNetwork,
+    network::{DistTonicNetwork, serialize_task_id},
     protobuf::{self, dist_tonic_service_server::DistTonicServiceServer},
     server::{DistTonicServer, parse_task_id},
 };
 use datafusion_proto::physical_plan::DefaultPhysicalExtensionCodec;
 use futures::{Stream, StreamExt, TryStreamExt};
+use log::debug;
 use prost::Message;
 use tonic::{Request, Response, Status, Streaming, metadata::MetadataValue, transport::Server};
 use uuid::Uuid;
@@ -51,11 +61,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .add_service(DistTonicServiceServer::new(dist_tonic_service))
         .serve("[::1]:50050".parse()?);
 
-    let test_flight_service = TestFlightService {
+    let test_flight_sql_service = TestFlightSqlService {
+        ctx: ctx.clone(),
         runtime: runtime.clone(),
     };
     let test_flight_server = Server::builder()
-        .add_service(FlightServiceServer::new(test_flight_service))
+        .add_service(FlightServiceServer::new(test_flight_sql_service))
         .serve("[::1]:50051".parse()?);
 
     tokio::select! {
@@ -73,67 +84,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-struct TestFlightService {
+struct TestFlightSqlService {
+    ctx: SessionContext,
     runtime: Arc<DistRuntime>,
 }
 
 type BoxedFlightStream<T> = Pin<Box<dyn Stream<Item = Result<T, Status>> + Send + 'static>>;
 
 #[tonic::async_trait]
-impl FlightService for TestFlightService {
-    type DoActionStream = BoxedFlightStream<arrow_flight::Result>;
-    type DoExchangeStream = BoxedFlightStream<FlightData>;
-    type DoGetStream = BoxedFlightStream<FlightData>;
-    type DoPutStream = BoxedFlightStream<PutResult>;
-    type HandshakeStream = BoxedFlightStream<HandshakeResponse>;
-    type ListActionsStream = BoxedFlightStream<ActionType>;
-    type ListFlightsStream = BoxedFlightStream<FlightInfo>;
+impl FlightSqlService for TestFlightSqlService {
+    type FlightService = Self;
 
-    async fn do_get(
-        &self,
-        request: Request<Ticket>,
-    ) -> Result<Response<Self::DoGetStream>, Status> {
-        let ticket = request.into_inner();
-        let mut buf = Cursor::new(ticket.ticket);
-        let proto_task_id = protobuf::TaskId::decode(&mut buf).unwrap();
-        let task_id = parse_task_id(proto_task_id);
-        let stream = self
-            .runtime
-            .execute_local(task_id)
-            .await
-            .map_err(|e| Status::from_error(Box::new(e)))?;
-        let stream = stream
-            .map_err(|e| FlightError::ExternalError(Box::new(e)))
-            .boxed();
-
-        let write_options = IpcWriteOptions::default();
-        let flight_data_stream = FlightDataEncoderBuilder::new()
-            .with_options(write_options)
-            .build(stream)
-            .map_err(|err| Status::from_error(Box::new(err)));
-        Ok(Response::new(
-            Box::pin(flight_data_stream) as Self::DoGetStream
-        ))
-    }
-
-    async fn get_schema(
-        &self,
-        _request: Request<FlightDescriptor>,
-    ) -> Result<Response<SchemaResult>, Status> {
-        Err(Status::unimplemented("get_schema"))
-    }
-
-    async fn get_flight_info(
-        &self,
-        _request: Request<FlightDescriptor>,
-    ) -> Result<Response<FlightInfo>, Status> {
-        Err(Status::unimplemented("get_flight_info"))
-    }
-
-    async fn handshake(
+    async fn do_handshake(
         &self,
         _request: Request<Streaming<HandshakeRequest>>,
-    ) -> Result<Response<Self::HandshakeStream>, Status> {
+    ) -> Result<Response<BoxedFlightStream<HandshakeResponse>>, Status> {
         let token = Uuid::new_v4();
 
         let result = HandshakeResponse {
@@ -151,45 +116,121 @@ impl FlightService for TestFlightService {
         Ok(resp)
     }
 
-    async fn list_flights(
+    async fn do_get_fallback(
         &self,
-        _request: Request<Criteria>,
-    ) -> Result<Response<Self::ListFlightsStream>, Status> {
-        Err(Status::unimplemented("list_flights"))
+        _request: Request<Ticket>,
+        message: Any,
+    ) -> Result<Response<<Self as FlightService>::DoGetStream>, Status> {
+        debug!("do_get_fallback type_url: {}", message.type_url);
+        if !message.is::<protobuf::TaskId>() {
+            Err(Status::unimplemented(format!(
+                "do_get: The defined request is invalid: {}",
+                message.type_url
+            )))?
+        }
+
+        let proto_task_id: protobuf::TaskId = message
+            .unpack()
+            .map_err(|e| Status::internal(format!("{e:?}")))?
+            .ok_or_else(|| Status::internal("Expected an TaskId but got None!"))?;
+
+        let task_id = parse_task_id(proto_task_id);
+
+        let stream = self
+            .runtime
+            .execute_local(task_id)
+            .await
+            .map_err(|e| Status::from_error(Box::new(e)))?;
+        let stream = stream
+            .map_err(|e| FlightError::ExternalError(Box::new(e)))
+            .boxed();
+
+        let write_options = IpcWriteOptions::default();
+        let flight_data_stream = FlightDataEncoderBuilder::new()
+            .with_options(write_options)
+            .build(stream)
+            .map_err(|err| Status::from_error(Box::new(err)));
+        Ok(Response::new(
+            Box::pin(flight_data_stream) as <Self as FlightService>::DoGetStream
+        ))
     }
 
-    async fn do_put(
+    async fn get_flight_info_statement(
         &self,
-        _request: Request<Streaming<FlightData>>,
-    ) -> Result<Response<Self::DoPutStream>, Status> {
-        Err(Status::unimplemented("do_put"))
-    }
-
-    async fn do_action(
-        &self,
-        _request: Request<Action>,
-    ) -> Result<Response<Self::DoActionStream>, Status> {
-        Err(Status::unimplemented("do_action"))
-    }
-
-    async fn list_actions(
-        &self,
-        _request: Request<Empty>,
-    ) -> Result<Response<Self::ListActionsStream>, Status> {
-        Err(Status::unimplemented("list_actions"))
-    }
-
-    async fn do_exchange(
-        &self,
-        _request: Request<Streaming<FlightData>>,
-    ) -> Result<Response<Self::DoExchangeStream>, Status> {
-        Err(Status::unimplemented("do_exchange"))
-    }
-
-    async fn poll_flight_info(
-        &self,
+        query: CommandStatementQuery,
         _request: Request<FlightDescriptor>,
-    ) -> Result<Response<PollInfo>, Status> {
-        Err(Status::unimplemented("poll_flight_info"))
+    ) -> Result<Response<FlightInfo>, Status> {
+        let df = self
+            .ctx
+            .sql(&query.query)
+            .await
+            .map_err(|e| Status::from_error(Box::new(e)))?;
+        let plan = df
+            .create_physical_plan()
+            .await
+            .map_err(|e| Status::from_error(Box::new(e)))?;
+        debug!(
+            "Create Physical Plan: {}",
+            DisplayableExecutionPlan::new(plan.as_ref()).indent(true)
+        );
+        let schema = plan.schema();
+
+        let (_job_id, stage0_task_distribution) = self
+            .runtime
+            .submit(plan)
+            .await
+            .map_err(|e| Status::from_error(Box::new(e)))?;
+
+        let endpoints = build_flight_endpoints(stage0_task_distribution);
+        let schema_bytes = schema_to_ipc(schema)?;
+        let flight_desc = FlightDescriptor {
+            r#type: DescriptorType::Cmd.into(),
+            cmd: Vec::new().into(),
+            path: vec![],
+        };
+
+        let info = FlightInfo {
+            schema: schema_bytes.into(),
+            flight_descriptor: Some(flight_desc),
+            endpoint: endpoints,
+            total_records: -1,
+            total_bytes: -1,
+            ordered: false,
+            app_metadata: Default::default(),
+        };
+        Ok(Response::new(info))
     }
+
+    async fn register_sql_info(&self, _id: i32, _result: &SqlInfo) {}
+}
+
+fn build_flight_endpoints(task_distribution: HashMap<TaskId, NodeId>) -> Vec<FlightEndpoint> {
+    let mut endpoints = Vec::new();
+    for (task_id, node_id) in task_distribution {
+        let proto_task_id = serialize_task_id(task_id);
+        let buf = proto_task_id.as_any().encode_to_vec();
+        let ticket = Ticket { ticket: buf.into() };
+
+        let location = format!("grpc://{}:{}", node_id.host, node_id.port);
+
+        let endpoint = FlightEndpoint::new()
+            .with_ticket(ticket)
+            .with_location(location);
+        endpoints.push(endpoint);
+    }
+    endpoints
+}
+
+#[allow(clippy::result_large_err)]
+fn schema_to_ipc(schema: SchemaRef) -> Result<Vec<u8>, Status> {
+    let options = IpcWriteOptions::default();
+    let pair = SchemaAsIpc::new(&schema, &options);
+    let data_gen = IpcDataGenerator::default();
+    let mut dictionary_tracker = DictionaryTracker::new(true);
+    let encoded_data =
+        data_gen.schema_to_bytes_with_dictionary_tracker(pair.0, &mut dictionary_tracker, pair.1);
+    let mut schema_bytes = vec![];
+    datafusion::arrow::ipc::writer::write_message(&mut schema_bytes, encoded_data, pair.1)
+        .map_err(|e| Status::internal(format!("Error encoding schema: {e}")))?;
+    Ok(schema_bytes)
 }
--- networks/tonic/src/lib.rs
+++ networks/tonic/src/lib.rs
@@ -1,3 +1,18 @@
 pub mod network;
 pub mod protobuf;
 pub mod server;
+
+use prost::Message;
+
+impl arrow_flight::sql::ProstMessageExt for protobuf::TaskId {
+    fn type_url() -> &'static str {
+        "type.googleapis.com/arrow.flight.protocol.sql.TaskId"
+    }
+
+    fn as_any(&self) -> arrow_flight::sql::Any {
+        arrow_flight::sql::Any {
+            type_url: protobuf::TaskId::type_url().to_string(),
+            value: self.encode_to_vec().into(),
+        }
+    }
+}
