52defcf8dfde34e83a7cf2cd98e98f3642b6ddf1
张林伟 <lewiszlw520@gmail.com>
2025-12-23 14:10:43 +0800

    Add sqllogictest

---
+++ integration-tests/tests/sqllogictest.rs
@@ -0,0 +1,23 @@
+use datafusion_dist_integration_tests::setup_containers;
+use sqllogictest_flightsql::runner::FlightSqlDB;
+
+#[tokio::test]
+async fn sqllogictest() -> Result<(), Box<dyn std::error::Error>> {
+    setup_containers().await;
+
+    let mut tester = sqllogictest::Runner::new(|| async {
+        FlightSqlDB::new_from_endpoint("dist", "http://localhost:50061").await
+    });
+
+    let mut slts_dir = tokio::fs::read_dir(format!("tests/slts")).await?;
+    while let Some(entry) = slts_dir.next_entry().await? {
+        if entry.file_type().await?.is_file() {
+            println!(
+                "======== start to run file {} ========",
+                entry.file_name().to_str().unwrap()
+            );
+            tester.run_file_async(entry.path()).await?;
+        }
+    }
+    Ok(())
+}
